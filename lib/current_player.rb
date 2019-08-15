9813a5c060a904790b6a9b185f48fdd52212dabc
Naveed Ganjani <nganjani@protonmail.com>
2019-08-15 20:31:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,5 +7,14 @@ def turn_count(board)
 end
 
 def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
+
+#bonus
+def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
